554b424dc63994a9677c969c939d1c1efed2bba6
tobitobit0bi <tobias.coman@hotmail.com>
2022-09-22 17:22:33 -0300

    Ejercicio 3 terminado

--- CasillaEjer3/Ejercicio3.cs
+++ CasillaEjer3/Ejercicio3.cs
@@ -5,107 +5,114 @@ using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace CasillaEjer3
 {
-    internal class Program
+
+    public class Password
     {
 
-        public class Password
+        private int longitud;
+
+        private string contraseña;
+
+        public int getLongitud()
         {
+            return longitud;
+        }
+        public void setLongitud(int longitud)
+        {
+            this.longitud = longitud;
+        }
 
-            private static int longdefault = 8;
-            private int longitud;
-            private string contraseña;
-
-            public int getLongitud()
+        public string getContraseña()
+        {
+            return contraseña;
+        }
+        public string generaPassword()
+        {
+            string password = "";
+            char[] letras = {'Q','W','E','E','R','T','Y','U','I', 'O', 'P', 'A','S','D','F','G', 'H', 'J', 'K', 'L'
+            ,'Ñ','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l'
+            ,'ñ','z','x','c','v','b','n','m','0','1','2','3','4','5','6','7','8','9'};
+            Random random = new Random();
+            for (int i = 0; i < this.longitud; i++)
             {
-                return longitud;
+                int letraR = random.Next(letras.Length);
+                password = password + letras[letraR];
             }
+            return password;
+        }
 
-            public string getContrase()
+        public bool esFuerte()
+        {
+            int cuentanumeros = 0;
+            int cuentaminusculas = 0;
+            int cuentamayusculas = 0;
+            for (int i = 0; i < contraseña.Length; i++)
             {
-                return contraseña;
-            }
-
-            public void setLongitud(int longitud)
-            {
-                this.longitud = longitud;
-            }
-
-            public Password()
-            {
-                this.longitud = 0;
-            }
-
-            public Password(int longitud)
-            {
-                this.longitud = longitud;
-                contraseña = GenerarPassword();
-            }
-
-            public string GenerarPassword()
-            {
-                string password = "";
-                for (int i = 0; i < longitud; i++)
+                if (contraseña[i] >= 97 && contraseña[i] <= 122)
                 {
-                    Random random = new Random();
-                    int eleccion = random.Next((1) * 3 + 1);
-
-                    if (eleccion == 1)
-                    {
-                        char minus;
-                    }
-                    else if (eleccion == 2)
-                    {
-                        char mayus;
-                    }
-                    else
-                    {
-                        char nums;
-                    }
-                }
-                return password;
-            }
-
-            public bool esFuerte()
-            {
-                int cuentanums = 0;
-                int cuentaminus = 0;
-                int cuentamayus = 0;
-                for (int i = 0; i < contraseña.Length; i++)
-                {
-                    if (contraseña[i] >= 97 && contraseña[i] <= 122)
-                    {
-                        cuentaminus += 1;
-                    }
-                    else
-                    {
-                        if (contraseña[i] >= 65 && contraseña[i] <= 90)
-                        {
-                            cuentamayus += 1;
-                        }
-                        else
-                        {
-                            cuentanums += 1;
-                        }
-                    }
-                }
-                if (cuentanums >= 5 && cuentaminus >= 1 && cuentamayus >= 2)
-                {
-                    return true;
+                    cuentaminusculas += 1;
                 }
                 else
                 {
-                    return false;
+                    if (contraseña[i] >= 65 && contraseña[i] <= 90)
+                    {
+                        cuentamayusculas += 1;
+                    }
+                    else
+                    {
+                        cuentanumeros += 1;
+                    }
                 }
             }
 
-            static void Main(string[] args)
+            if (cuentanumeros > 5 && cuentaminusculas > 1 && cuentamayusculas > 2)
             {
-
-                Console.Readkey();
-
+                return true;
             }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Password(int longitud)
+        {
+            this.longitud = longitud;
+            contraseña = generaPassword();
+        }
+        public Password(object longitud)
+        {
+            this.longitud = (int)longitud;
+            contraseña = generaPassword();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un tamaño para el array: ");
+            string tamanioIngresado = Console.ReadLine();
+            int tamanioParse = Int32.Parse(tamanioIngresado);
+
+            Console.Write("Introduce la longitud del password: ");
+            string longIngresado = Console.ReadLine();
+            int longParse = Int32.Parse(longIngresado);
+
+            Password[] listaPasswords = new Password[tamanioParse];
+            bool[] fortalezaPassword = new bool[tamanioParse];
+
+            for (int i = 0; i < listaPasswords.Length; i++)
+            {
+                Password pass = new Password(longParse);
+                listaPasswords[i] = pass;
+                fortalezaPassword[i] = listaPasswords[i].esFuerte();
+                Console.WriteLine(listaPasswords[i].getContraseña() + " " + fortalezaPassword[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
